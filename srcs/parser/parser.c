9c04fc6c5d4f341a297d1478ef76f4a488dd2e23
XilianRose <m.stegeman@me.com>
2023-08-24 18:08:38 +0200

    add rdrnode

--- srcs/parser/parser.c
+++ srcs/parser/parser.c
@@ -6,43 +6,87 @@
 /*   By: mstegema <mstegema@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/08/01 14:24:50 by cschabra      #+#    #+#                 */
-/*   Updated: 2023/08/24 16:57:11 by mstegema      ########   odam.nl         */
+/*   Updated: 2023/08/24 18:07:40 by mstegema      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-t_scmd_list	*make_scmdlist(void)
+static t_scmd_list	*make_rdrnode(t_list *tokens, t_scmd_list **scmds)
 {
+	t_scmd_list		*node;
+	t_rdr			*rdr;
+	t_token			*token;
+
+	rdr = allocate_mem_redirect(rdr, )
+	//how to allocate rdr struct?
+	while (tokens->next != NULL && tokens->next != '|')
+	{
+		token = tokens->content;
+		if (token->type == REDIRECTION_TOKEN)
+		{
+			if (ft_strncmp(token->data, ">", 2) == 0)
+				rdr->type = RDR_OUTPUT;
+				// rdr = allocate_mem_redirect(rdr, tokens.next.content.data, rdr_ouput)
+			if (ft_strncmp(token->data, "<", 2) == 0)
+				rdr->type = RDR_INPUT;
+			if (ft_strncmp(token->data, ">>", 3) == 0)
+				rdr->type = RDR_APPEND;
+			if (ft_strncmp(token->data, "<<", 3) == 0)
+				rdr->type = HERE_DOC;
+		}
+		else
+	}
+
 }
 
-size_t	calc_smds(char **ui_array)
+static size_t	*make_scmdlist(t_list *tokens, t_scmd_list **scmds)
 {
-	t_cmd	*command;
+	t_scmd_list		*node;
+	t_rdr			*rdr;
+	t_cmd			*cmd;
+	t_token			*token;
+	bool			has_rdr;
 
-	res = 1;
-	i = 0;
-	while (ui_array)
+	has_rdr = false;
+	while (tokens->next != NULL && tokens->next != '|')
 	{
-		if (ui_array[i][0] == '|')
-			res++;
-		i++;
+		token = tokens->content;
+		if (token->type == CMD_OR_FILE_TOKEN && has_rdr == false)
+		{
+			node = make_rdrnode(tokens, scmds);
+		}
+		if (token->type == REDIRECTION_TOKEN)
+		{
+			has_rdr = true;
+		}
+
 	}
-	return (res);
+	return (0);
 }
 
-size_t	calc_smds(char **ui_array)
+static size_t	make_cmdlist(t_list *tokens, t_list **cmds)
 {
-	size_t	res;
-	size_t	i;
+	t_list		*node;
+	t_scmd_list	*scmds;
 
-	res = 1;
-	i = 0;
-	while (ui_array)
+	while (tokens->next != NULL)
 	{
-		if (ui_array[i][0] == '|')
-			res++;
-		i++;
+		make_scmdlist(tokens, &scmds);
+		node = ft_lstnew(scmds);
+		if (!node)
+			return (ft_lstclear(cmds, &free), 1);
 	}
-	return (res);
+	return (0);
+}
+
+t_list	*parse(const char *user_input)
+{
+	t_list	*tokens;
+	t_list	*cmds;
+
+	tokens = NULL;
+	tokens = tokenisation(user_input);
+	make_cmdlist(tokens, &cmds);
+
 }
