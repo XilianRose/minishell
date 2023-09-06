581553685641096fddcea546e9b8cabbdc352e82
Michelle Stegeman <mstegema@f1r1s3.codam.nl>
2023-09-06 14:16:08 +0200

    fixed merge conflict

--- srcs/main.c
+++ srcs/main.c
@@ -6,53 +6,11 @@
 /*   By: mstegema <mstegema@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/04/11 17:02:44 by cschabra      #+#    #+#                 */
-/*   Updated: 2023/09/06 14:14:52 by mstegema      ########   odam.nl         */
+/*   Updated: 2023/09/06 14:15:15 by mstegema      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-
-static void	print_commandlist(t_list *lst)
-{
-	t_scmd_list	*scmd;
-	t_cmd		*cmd;
-	t_rdr		*rdr;
-	int			i;
-	int			j;
-
-	i = 1;
-	while (lst != NULL)
-	{
-		printf("______________________________________________________________________\n");
-		printf("scmd #%i\n", i);
-		scmd = lst->content;
-		while (scmd != NULL)
-		{
-			printf("node type:	[%d]\n", scmd->type);
-			if (scmd->type == CMD)
-			{
-				cmd = scmd->data;
-				printf("path:		%s\n", cmd->path);
-				j = 0;
-				while (cmd->arg[j] != NULL)
-				{
-					printf("args:		%s\n", cmd->arg[j]);
-					j++;
-				}
-				printf("builtin:	%d\n\n", cmd->builtin);
-			}
-			else if (scmd->type == RDR)
-			{
-				rdr = scmd->data;
-				printf("data:		%s\n", rdr->data);
-				printf("rdr type:	%d\n\n", rdr->type);
-			}
-			scmd = scmd->next;
-		}
-		lst = lst->next;
-		i++;
-	}
-}
 
 static void	print_commandlist(t_list *lst)
 {
