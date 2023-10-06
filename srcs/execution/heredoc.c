93f0967c0d9f741abc1c36db58c4661507da9862
WriteSomeStuff <cheyennebune@gmail.com>
2023-10-06 18:47:09 +0200

    add working heredoc expansion

--- srcs/execution/heredoc.c
+++ srcs/execution/heredoc.c
@@ -12,38 +12,77 @@
 
 #include "minishell.h"
 
-// static void	ft_expand(void)
-// {
-// 	return ;
-// }
+static char	*ft_expand_loop(t_init *process, char *str)
+{
+	char	*temp;
+	char	*final;
 
-// static bool	ft_read_input(char *data, int32_t datalen, int32_t *fd, bool expand)
-// {
-// 	char	*temp;
-// 	size_t	templen;
+	final = expand_data(str, process->env);
+	while (ft_strchr(final, '$'))
+	{
+		temp = expand_data(final, process->env);
+		free(final);
+		final = malloc(sizeof(char) * (ft_strlen(temp) + 1));
+		if (!final)
+			return (perror("BabyBash"), free(temp), NULL);
+		ft_memmove(final, temp, ft_strlen(temp) + 1);
+		free(temp);
+		temp = NULL;
+	}
+	return (final);
+}
 
-// 	while (1)
-// 	{
-// 		temp = readline("> ");
-// 		if (!temp)
-// 			return (false);
-// 		if (!ft_strncmp(temp, data, datalen))
-// 		{
-// 			free(temp);
-// 			break ;
-// 		}
-// 		if (expand == true)
-// 			ft_expand();
-// 		templen = ft_strlen(temp);
-// 		if (write(fd[1], temp, templen) == -1 || write(fd[1], "\n", 1) == -1)
-// 		{
-// 			perror("BabyBash");
-// 			return (free(temp), false);
-// 		}
-// 		free(temp);
-// 	}
-// 	return (true);
-// }
+static bool	ft_expand_check(t_init *process, int32_t *fd, bool expand, char *str)
+{
+	char	*final;
+
+	if (expand == true && ft_strchr(str, '$'))
+	{
+		final = ft_expand_loop(process, str);
+		if (!final)
+			return (false);
+		if (write(fd[1], final, ft_strlen(final)) == -1 || write(fd[1], "\n", 1) == -1)
+		{
+			perror("BabyBash");
+			return (free(final), false);
+		}
+		free(final);
+	}
+	else
+	{
+		if (write(fd[1], str, ft_strlen(str)) == -1 || write(fd[1], "\n", 1) == -1)
+		{
+			perror("BabyBash");
+			return (false);
+		}
+	}
+	return (true);
+}
+
+static bool	ft_read_input(t_init *process, char *data, int32_t *fd, \
+	bool expand)
+{
+	char	*str;
+	size_t	datalen;
+
+	datalen = ft_strlen(data);
+	while (1)
+	{
+		str = readline("> ");
+		if (!str)
+			return (false);
+		if (!ft_strncmp(str, data, (datalen + 1)))
+		{
+			free(str);
+			break ;
+		}
+		if (!ft_expand_check(process, fd, expand, str))
+			return (free(str), false);
+		free(str);
+		str = NULL;
+	}
+	return (true);
+}
 
 static void	ft_remove_quotes(char *data)
 {
@@ -66,84 +105,12 @@ static void	ft_remove_quotes(char *data)
 	}
 }
 
-// bool	ft_heredoc(t_init *process, char *data)
-// {
-// 	int32_t	fd[2];
-// 	size_t	datalen;
-// 	bool	expand;
-
-// 	expand = true;
-// 	if (pipe(fd) == -1)
-// 		return (ft_throw_error(process, errno), false);
-// 	datalen = ft_strlen(data);
-// 	if (ft_strchr(data, '"') || ft_strchr(data, '\''))
-// 	{
-// 		ft_remove_quotes(data);
-// 		expand = false;
-// 	}
-// 	if (!ft_read_input(data, datalen, fd, expand))
-// 	{
-// 		process->errorcode = 1;
-// 		if (close(fd[0]) == -1 || close(fd[1]) == -1)
-// 			ft_throw_error(process, errno);
-// 		return (false);
-// 	}
-// 	if (dup2(fd[0], STDIN_FILENO) == -1 || close(fd[0]) == -1 || \
-// 		close(fd[1]) == -1)
-// 		return (ft_throw_error(process, errno), false);
-// 	return (true);
-// }
-
-static bool	ft_read_input(t_init *process, char *data, int32_t *fd, \
-	bool expand)
-{
-	char	*temp;
-	char	*final;
-	size_t	datalen;
-
-	datalen = ft_strlen(data);
-	while (1)
-	{
-		temp = readline("> ");
-		if (!temp)
-			return (false);
-		if (!ft_strncmp(temp, data, (datalen + 1)))
-		{
-			free(temp);
-			break ;
-		}
-		if (expand == true && ft_strchr(temp, '$'))
-		{
-			final = expand_data(temp, process->env);
-			free(temp);
-			if (write(fd[1], final, ft_strlen(final)) == -1 || write(fd[1], "\n", 1) == -1)
-			{
-				perror("BabyBash");
-				return (false);
-			}
-			free (final);
-			final = NULL;
-		}
-		else
-		{
-			if (write(fd[1], temp, ft_strlen(temp)) == -1 || write(fd[1], "\n", 1) == -1)
-			{
-				perror("BabyBash");
-				return (free(temp), false);
-			}
-			free(temp);
-		}
-	}
-	return (true);
-}
-
 bool	ft_heredoc(t_init *process, char *data)
 {
 	int32_t	fd[2];
 	bool	expand;
 
 	expand = true;
-	printf("%s\n", data);
 	if (pipe(fd) == -1)
 		return (ft_throw_error(process, errno), false);
 	if (ft_strchr(data, '"') || ft_strchr(data, '\''))
