965be21d3f877c4938ca3b2b599d795699e63609
Cheyenne Schabracq <cschabra@f0r3s9.codam.nl>
2023-12-12 15:27:36 +0100

    fix when no cmd passed to child

--- srcs/execution/childprocess.c
+++ srcs/execution/childprocess.c
@@ -6,7 +6,7 @@
 /*   By: cschabra <cschabra@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/05/02 13:32:13 by cschabra      #+#    #+#                 */
-/*   Updated: 2023/11/15 16:41:11 by cschabra      ########   odam.nl         */
+/*   Updated: 2023/12/12 15:26:08 by cschabra      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 void	ft_wait_for_last_child(t_init *process)
 {
 	ft_close_fds(process);
-	waitpid(process->ids[process->nr_of_cmds - 1], &process->status, 0);
+	waitpid(process->ids[process->i - 1], &process->status, 0);
 	if (WIFEXITED(process->status))
 		process->errorcode = WEXITSTATUS(process->status);
 	else if (WIFSIGNALED(process->status))
@@ -42,6 +42,11 @@ static void	ft_child_free(t_list *lst, t_init *process)
 
 static void	ft_child_process(t_list *lst, t_init *process)
 {
+	if (!process->cmd)
+	{
+		ft_close_fds(process);
+		exit(process->errorcode);
+	}
 	if (!process->fdout && ((!process->i && process->nr_of_cmds > 1) || \
 		(process->i != (process->nr_of_cmds - 1))))
 	{
@@ -81,18 +86,15 @@ static void	ft_find_cmd(t_scmd_list *lst, t_init *process)
 void	ft_create_child(t_list *lst, t_init *process)
 {
 	ft_find_cmd(lst->content, process);
-	if (process->cmd)
+	process->ids[process->i] = fork();
+	if (process->ids[process->i] == -1)
 	{
-		process->ids[process->i] = fork();
-		if (process->ids[process->i] == -1)
-		{
-			ft_throw_error(process, errno);
-			return ;
-		}
-		if (process->ids[process->i] == 0)
-			ft_child_process(lst, process);
-		process->i++;
+		ft_throw_error(process, errno);
+		return ;
 	}
+	if (process->ids[process->i] == 0)
+		ft_child_process(lst, process);
+	process->i++;
 	process->heredoc = false;
 	process->fdout = 0;
 	process->fdin = 0;
